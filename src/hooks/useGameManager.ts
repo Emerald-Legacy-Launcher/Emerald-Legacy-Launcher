0d215223c54ca97a4e223d59fb690f6181affbc9
/home/neo <158327205+neoapps-dev@users.noreply.github.com>
2026-04-02 22:57:12 +0300

    feat: Add LCRE logo, and a better description (#33)

--- src/hooks/useGameManager.ts
+++ src/hooks/useGameManager.ts
@@ -15,9 +15,9 @@ const BASE_EDITIONS = [
   {
     id: "revelations",
     name: "Legacy Revelations",
-    desc: "Hardcore, QoL, and a lot of patches for LCE.",
+    desc: "QoL, performance, hardcore mode, & security features for LCE.",
     url: "https://github.com/itsRevela/MinecraftConsoles/releases/download/Nightly/LCREWindows64.zip",
-    titleImage: "/images/minecraft_title_tu19.png"
+    titleImage: "/images/minecraft_title_revelations.png"
   },
   {
     id: "vanilla_tu19",
